e9e9801ebe22362e61cf701822a38195f2c36db2
rahul-github-id <164854654+rahul-github-id@users.noreply.github.com>
2024-06-18 10:01:46 +0000

    variable & basic datatype concept

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 6664444;  //one time declare 
+let accountEmail = 'ajit@gmail.com';  // mordern variables assigner 
+var accountPassword = '123456';  // it not current season used it only understand
+accountCity = 'Tinsukia'; // by default engine build var 
+let accountState; // it is undefined variable
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,14 @@
+'use strict' // treat JS code new version
+
+// alert('hello') // it not work in nodejs , it is for brower
+
+// premitive datatype
+// answer: A primitive type is predefined by the language and is named by a reserved keyword.
+//name are => String, Number, BigInt, Boolean, Null, Undefined, and Symbol.
+
+// string => '' , "" , ``;
+// number => 1234;
+// Boolean => true , false;
+// Undefined => unintaintaly assign or confuse for engine;
+// null => intaintaly assign; 
+// typeof => it help to find which type data store;
